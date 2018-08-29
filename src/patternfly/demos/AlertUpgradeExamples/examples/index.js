f4bc98f8aa600c852deae9e9eb1d135c4a48b3a6
Christie Molloy <cmolloy@cmolloy-mac.local>
2018-08-29 15:27:28 -0400

    fix pf3

--- src/patternfly/demos/AlertUpgradeExamples/examples/index.js
+++ src/patternfly/demos/AlertUpgradeExamples/examples/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
-import alertUpgradeExamplesSimpleExampleRaw from '!raw!./alert-upgrade-examples-simple-example.hbs';
+import alertUpgradeExamplespf3alertRaw from '!raw!./alert-upgrade-examples-pf3-alert.hbs';
+import alertUpgradeExamplespf3notificationRaw from '!raw!./alert-upgrade-example-pf3-notification.hbs';
 import alertUpgradeExamplespf4Raw from '!raw!./alert-upgrade-examples-pf4.hbs';
-import AlertupgradeexamplesSimpleExample from './alert-upgrade-examples-simple-example.hbs';
+import Alertupgradeexamplespf3alert from './alert-upgrade-examples-pf3-alert.hbs';
+import Alertupgradeexamplespf3notification from './alert-upgrade-example-pf3-notification.hbs';
 import Alertupgradeexamplespf4 from './alert-upgrade-examples-pf4.hbs';
 import alertUpgradeExamplesComplexExampleDoc from '../docs/alert-upgrade-examples-complex.md';
 import docs from '../docs/code.md';
@@ -12,20 +14,24 @@ import '../styles.scss';
 export const Docs = docs;
 
 export default () => {
-  const alertUpgradeExamplesSimpleExample = AlertupgradeexamplesSimpleExample();
+  const alertUpgradeExamplespf3alert = Alertupgradeexamplespf3alert();
+  const alertUpgradeExamplespf3notification = Alertupgradeexamplespf3notification();
   const alertUpgradeExamplespf4 = Alertupgradeexamplespf4();
 
   return (
     <Documentation docs={Docs}>
-      <Example
-        heading="Patternfly3 Alert and Notification are separated"
-        handlebars={alertUpgradeExamplesSimpleExampleRaw}
-        minHeight="20em"
-      >
-        {alertUpgradeExamplesSimpleExample}
+      <Example heading="Patternfly3 Alert" handlebars={alertUpgradeExamplespf3alertRaw} minHeight="20em">
+        {alertUpgradeExamplespf3alert}
       </Example>
       <Example
-        heading="Patternfly4 Alert (combines notification)"
+        heading="Patternfly3 Toast Notification"
+        handlebars={alertUpgradeExamplespf3notificationRaw}
+        minHeight="20em"
+      >
+        {alertUpgradeExamplespf3notification}
+      </Example>
+      <Example
+        heading="Patternfly4 Combines Alert and Notification"
         handlebars={alertUpgradeExamplespf4Raw}
         docs={alertUpgradeExamplesComplexExampleDoc}
       >
